5e3d3df2ec9d52e291e93aedad01bd2eaa0ae448
Maryan-said <maryan.said.farah@gmail.com>
2025-07-08 13:32:20 +0200

    added enhancement to the project  by improving the ui and fixing some bugs

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-  
+
     document.addEventListener('DOMContentLoaded', function () {
       const navbar = document.getElementById('navbar');
       const hamburger = document.getElementById('hamburgerBtn');
